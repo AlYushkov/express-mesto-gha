e7eed4dcee1026f58c0433ce90cdb6366178b1ad
AlYushkov <iushk19@yandex.ru>
2022-10-02 20:05:49 +0500

    fix tests

--- models/card.js
+++ models/card.js
@@ -2,39 +2,6 @@ const mongoose = require('mongoose');
 
 const { Schema, model } = require('mongoose');
 
-const User = require('./user.js');
-
-const likesSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-        minlength: 2,
-        maxlength: 30,
-    },
-    about:
-    {
-        type: String,
-        required: true,
-        minlength: 2,
-        maxlength: 30,
-    },
-    avatar:
-    {
-        type: String,
-        required: true,
-    },
-    _id:
-    {
-        type: String,
-        required: true,
-    },
-    cohort:
-    {
-        type: String,
-        required: true,
-    },
-})
-
 const cardSchema = new Schema({
     name: {
         type: String,
