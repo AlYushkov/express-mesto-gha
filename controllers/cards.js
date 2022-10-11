ebe32e72d712278755292dce16ac15268a6c4308
AlYushkov <iushk19@yandex.ru>
2022-10-11 16:09:53 +0500

    change cards

--- controllers/cards.js
+++ controllers/cards.js
@@ -3,11 +3,7 @@ const Card = require('../models/card');
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
-    // eslint-disable-next-line consistent-return
     .then((card) => {
-      if (!card) {
-        return Promise.reject(new Error('500'));
-      }
       res.send({ data: card });
     })
     .catch((e) => {
@@ -20,30 +16,22 @@ module.exports.createCard = (req, res) => {
 };
 
 module.exports.getCards = (req, res) => {
-  Card.find({}).populate(['owner', 'likes'])
-    // eslint-disable-next-line consistent-return
+  Card.find({}).populate(['owner', 'likes']
     .then((card) => {
       if (card.length === 0) {
-        return Promise.reject(new Error('300'));
-      }
-      res.send({ data: card });
-    })
-    .catch((e) => {
-      if (e.message === '300') {
         res.status(300).send({ msessage: 'Нет данных' });
       } else {
-        res.status(500).send({ msessage: 'Ошибка на сервере' });
+        res.send({ data: card });
       }
-    });
+    })
+    .catch(() => {
+      res.status(500).send({ msessage: 'Ошибка на сервере' });
+    }));
 };
 
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
-    // eslint-disable-next-line consistent-return
     .then((card) => {
-      if (!card) {
-        return Promise.reject(new Error('500'));
-      }
       res.send({ data: card });
     })
     .catch((e) => {
@@ -61,17 +49,11 @@ module.exports.likeCard = (req, res) => {
     { $addToSet: { likes: req.user._id } },
     { new: true },
   )
-    // eslint-disable-next-line consistent-return
     .then((card) => {
-      if (!card) {
-        return Promise.reject(new Error('404'));
-      }
       res.send({ data: card });
     })
     .catch((e) => {
-      if (e.message === '404') {
-        res.status(404).send({ message: 'Нет данных' });
-      } else if (e.name === 'CastError') {
+      if (e.name === 'CastError') {
         res.status(400).send({ message: 'Некорректные данные' });
       } else {
         res.status(500).send({ msessage: 'Ошибка на сервере' });
@@ -85,17 +67,11 @@ module.exports.dislikeCard = (req, res) => {
     { $pull: { likes: req.user._id } },
     { new: true },
   )
-    // eslint-disable-next-line consistent-return
     .then((card) => {
-      if (!card) {
-        return Promise.reject(new Error('404'));
-      }
       res.send({ data: card });
     })
     .catch((e) => {
-      if (e.message === '404') {
-        res.status(404).send({ message: 'Нет данных' });
-      } else if (e.name === 'CastError') {
+      if (e.name === 'CastError') {
         res.status(400).send({ message: 'Некорректные данные' });
       } else {
         res.status(500).send({ msessage: 'Ошибка на сервере' });
